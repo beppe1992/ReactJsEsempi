a6f714a8df36e802a84d230eacaf67437039d444
Giuseppe <giuseppe.giordano1992@gmail.com>
2016-04-15 17:28:06 +0200

    Form ReactJs

--- HelloWorldExample/www/app/jsx/HelloWorld.js
+++ HelloWorldExample/www/app/jsx/HelloWorld.js
@@ -1,12 +1,30 @@
-var MyComponent = React.createClass({
-    render: function(){
-        return (
-            <h1>Hello, world!!!!</h1>
-        );
-    }
+var MyForm = React.createClass({
+  getInitialState: function() {
+     return {
+         input: '',
+         output: ''
+     };
+   },
+   updateInput(event){
+      this.setState({
+           input: event.target.value
+      })
+    },
+    submit(event){
+      this.state.output = this.state.input,
+      this.setState({
+        input : ''
+      })
+   },
+   render: function(){
+       return(
+         <div>
+           <input type="text" onChange={this.updateInput} value={this.state.input} />
+           <button type="submit" onClick={this.submit}>Ricerca</button>
+           <h1>Ciao {this.state.output}!!!! </h1>
+         </div>
+       );
+     }
 });
 
-ReactDOM.render(
-    <MyComponent/>,
-    document.getElementById('myDiv')
-);
+ReactDOM.render(<MyForm />, document.getElementById('MyForm'));
